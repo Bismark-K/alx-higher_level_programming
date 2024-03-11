9b4da88f08e1c9d1935e9d8d9aef2d02cad10264
dz1dzor <kbismarkkwaku@gmail.com>
2024-03-11 21:54:46 +0000

    Debugged

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-const printTimes = process.argv[2];
-const checkArg = parseInt(printTimes);
-
-squares(checkArg);
-
-function squares (size) {
-  // print the squares
-  for (let i = 0; i < size; i++) {
-    let r = '';
-    for (let j = 0; j < size; j++) {
-      r += 'X';
-    }
-    console.log(r);
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
   }
 }
