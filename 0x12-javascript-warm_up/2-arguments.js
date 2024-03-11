b5689ccb358a7e62b1159fa5cc2f50e11e36cf80
dz1dzor <kbismarkkwaku@gmail.com>
2024-03-11 18:11:44 +0000

    Debugged still

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,3 @@
 #!/usr/bin/node
 const count = process.argv.length;
-
-if (count === 2) {
-  console.log('No arguments');
-} else if (count === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
+console.log(count === 2 ? 'No arguments' : count === 3 ? 'Argument found' : 'Arguments found');
