fed37264255611be7fdc96b0deafb0ac35237d39
dz1dzor <kbismarkkwaku@gmail.com>
2024-03-11 21:51:35 +0000

    Solution to task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-const x = parseInt(process.argv[2]);
-let i = 0;
-
-// Checking to see if the argument can be converted
-if (!isNaN(x)) {
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  // printing C is fun x times
-  while (i < x) {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
-    i++;
   }
 }
